145b02dfe118065c5dadadb90bf699f1dc26a7c8
jcroosvelt <j.roosvelt11@gmail.com>
2026-09-18 03:00:17 -0400

    Delete about.js

--- about.js
+++
@@ -1,7 +0,0 @@
-// assets/js/app/pages/about.js
-// TODO: reprendre le rendu exact de la fonction correspondante du prototype
-// pour cette page, sans changer le design.
-
-export async function render(root) {
-  root.innerHTML = `<h1>A propos</h1>`;
-}
